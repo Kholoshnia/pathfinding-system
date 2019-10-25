ad3d430143bd7d68b754d65586eda69520a5ccbe
Vadim Kholoshnya <vhd-ru@yandex.ru>
2019-10-25 23:54:50 +0300

    Delete "Additionals" in UNITY C# folder

--- Source Files/UNITY C#/Additionals/Drone/Assets/Scripts/Information.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Information : MonoBehaviour
-{
-    public float GetDistance(GameObject obj) { return Vector3.Distance(obj.transform.position, transform.position); }
-}
