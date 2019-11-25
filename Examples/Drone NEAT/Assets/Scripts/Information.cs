ad623f4f669d2d2fba9f6a38aa65808f63ee0bb9
Vadim Kholoshnya <vhd-ru@yandex.ru>
2019-11-25 11:28:54 +0300

    Add Map Creator

---
+++ Examples/Drone NEAT/Assets/Scripts/Information.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Information : MonoBehaviour
+{
+    public float GetDistance(GameObject obj) { return Vector3.Distance(obj.transform.position, transform.position); }
+}
